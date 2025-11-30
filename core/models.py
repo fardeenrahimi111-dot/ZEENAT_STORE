c6fcb3e850762db1f42aae6c626394c7742c6cdb
Fardeen Rahimi <fardeen.rahimi111@gmail.com>
2025-11-30 09:35:15 +0430

    initial django project with repoting libs

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+class product(models.Model):
+    
+      name = models.CharField(max_length=100)
+      price = models.DecimalField(max_digits=10, decimal_places=2)
+      quantity = models.IntegerField()
+      description = models.TextField(blank=True,null=True)
+      image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+
+      def __str__(self):
+        return self.name
